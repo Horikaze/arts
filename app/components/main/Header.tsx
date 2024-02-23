042f15e20511c5de0b27794327f8b3b5817fc00c
Horikaze <adrianyt659@gmail.com>
2024-02-23 21:47:52 +0100

    css update

--- app/components/main/Header.tsx
+++ app/components/main/Header.tsx
@@ -3,10 +3,10 @@ import { FaCoffee, FaTwitter } from "react-icons/fa";
 const supporters = ["123", "456"];
 export default function Header() {
   return (
-    <div className="h-36 border-b border-b-text/10 p-4 flex justify-between font-semibold">
+    <div className="h-28 border-b border-b-text/10 p-4 flex justify-between font-semibold">
       <div className="w-full h-full flex flex-col items-start gap-y-2">
         <div className="flex items-center justify-center gap-x-2">
-          <FaTwitter className="h-10 w-10" />
+          <FaTwitter className="size-8" />
           <a
             href="https://twitter.com/horikaze0"
             className="underline"
@@ -16,7 +16,7 @@ export default function Header() {
           </a>
         </div>
         <div className="flex items-center justify-center gap-x-2">
-          <FaCoffee className="h-10 w-10" />
+          <FaCoffee className="size-8" />
           <a
             href="https://ko-fi.com/horikaze"
             className="underline"
